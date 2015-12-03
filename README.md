c19deed71f96dc461e9eacfe647ecc52f3e95476
Kevin Greene <kevin@spantree.net>
2015-12-03 11:52:47 -0500

    Reworked many things, more rework to come

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Each player takes a Player Card (currently not made, just have an area for 5 cub
 
 ## Round Setup
 
-At the beginning of each round, shuffle the deck and turn over 4x cards, where x is the number of players.
+At the beginning of each round, shuffle the deck and turn over 3X cards, where x is the number of players.
 
 Choose a starting player.
 
@@ -18,23 +18,12 @@ Players then go around clockwise, selecting one of the cards to have for the nex
 
 ## Turns
 
-The starting player rolls 4 colored dice. Each player takes the colors that come up, and places it on their cards including the player card with 5 wilds. If they cannot, they are out for this round, and receive 0 points.
+The starting player rolls 4 colored dice. Each player takes the colors that come up, and places it on their cards including the player card with 5 wilds. If they cannot, they must pick an animal to overfed. Flip that card over, and place the cube on top. 
 
 After this occurs, players have the option to back out and score right then, or continue on. Repeat.
 
 After all players have failed or backed out, repeat Round Setup
 
+## Scoring
 
-### Attributions (Currently not in the product, but in the repo)
-
-Coal Trolley by David Chapman from the Noun Project
-
-Fire by Richard Cordero from the Noun Project
-
-Gas Tank by jon trillana from the Noun Project
-
-radioactive by Aha-Soft from the Noun Project
-
-Trash by trasnik from the Noun Project
-
-oil drum by Creative Stall from the Noun Project
+When a player scores, add all points from animals that are fully fed, and subtract 1 for each animal that was undernourished, 3 for each animal that was overfed, 5 for any animal that was not fed at all.
--- generator/index.js
+++ generator/index.js
@@ -31,7 +31,14 @@ var options = [
     "option6"
 ];
 
-var names = Moniker.generator([Moniker.adjective, Moniker.noun]);
+var adjectiveList = [
+    "Ornery",
+    "Hungry",
+    "Massive",
+    "Timid",
+    "Sleepy",
+    "Feral"
+];
 
 var buildChargeArray = function(chargeFrequencyArray, index, options){
     var charges = [];
@@ -42,10 +49,10 @@ var buildChargeArray = function(chargeFrequencyArray, index, options){
     return charges;
 };
 
-var createCards = function(pointValue, chargeFrequencyArray, overchargeFrequencyArray, options){
+var createCards = function(pointValue, chargeFrequencyArray, overchargeFrequencyArray, baseName, options){
     var cards = [];
     for(var index = 0; index < options.length; index++){
-        var name = names.choose();
+        var name = adjectiveList[index] + " " + baseName;
         var charge = buildChargeArray(chargeFrequencyArray, index, options);
         var overcharge = buildChargeArray(overchargeFrequencyArray, index, options);
         cards.push({
@@ -78,16 +85,22 @@ var convertCardToCsvLine = function(card, arrayLength) {
 
 
 var cards  = [].concat(
-    createCards(3, [0, 0, 1, 2], [0, 3], options),
-    createCards(4, [0, 0, 1, 3, 5], [0, 4], options),
-    createCards(5, [0, 0, 0, 2, 2, 2], [5, 5], options),
-    createCards(5, [0, 1, 2, 3, 4, 5], [1, 2], options),
-    createCards(1, [0, 0], [1, 2, 3, 4, 5], options),
-    createCards(2, [0, 1, 2], [3, 3], options),
-    createCards(6, [0, 0, 1, 2, 3, 3, 4], [0, 4, 5], options),
-    createCards(6, [0, 1, 1, 1, 5, 5, 5], [0, 2, 3, 4], options),
-    createCards(8, [0, 1, 1, 1, 2, 3, 4, 5], [0], options),
-    createCards(5, [0, 0, 0, 0], [1, 4, 5], options)
+    createCards(3, [0, 0, 1, 2], [0, 3], "Faerie", options),
+    createCards(4, [0, 0, 1, 3, 5], [0, 4], "Gremlin", options),
+    createCards(5, [0, 0, 0, 2, 2, 2], [5, 5], "Basilisk", options),
+    createCards(5, [0, 1, 2, 3, 4, 5], [1, 2], "Unicorn", options),
+    createCards(1, [0, 0], [1, 2, 3, 4, 5], "Wyvern", options),
+    createCards(2, [0, 1, 2], [3, 3], "Ogre", options),
+    createCards(6, [0, 0, 1, 2, 3, 3, 4], [0, 4, 5], "Pegasus", options),
+    createCards(6, [0, 1, 1, 1, 5, 5, 5], [0, 2, 3, 4], "Griffin", options),
+    createCards(8, [0, 1, 1, 1, 2, 3, 4, 5], [0], "Cockatrice", options),
+    createCards(5, [0, 0, 0, 0], [1, 4, 5], "Chimera", options),
+    createCards(1, [0, 1], [3], "Imp", options),
+    createCards(3, [0, 0, 1, 2], [1, 3, 4, 5], "Phoenix", options),
+    createCards(5, [0, 1, 1, 1], [1, 1, 1], "Harpy", options),
+    createCards(4, [0, 2, 2, 5], [3, 3, 4], "Roc", options),
+    createCards(4, [0, 3, 3, 4], [2, 3, 5, 5], "Genie", options),
+    createCards(10,[0, 0, 1, 1, 2, 2, 3, 3], [], "Dragon", options)
 );
 
 var wstream = fs.createWriteStream('containers.csv');
